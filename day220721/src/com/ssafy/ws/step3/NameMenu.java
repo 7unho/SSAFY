6a31d414adb8ea71a0da4cb1ecfd298841f35366
april2nds <rlawnsgh8395@naver.com>
2022-07-21 18:02:42 +0900

    [ADD] 220721 practice

---
+++ day220721/src/com/ssafy/ws/step3/NameMenu.java
@@ -0,0 +1,38 @@
+package com.ssafy.ws.step3;
+
+public class NameMenu {
+    static String[] names = new String[5];
+
+    public static void addName(String name){
+        for (int i = 0; i < 5; i ++ ){
+            if (names[i] == null) {
+                names[i] = name;
+                return;
+            }
+        }
+    }
+
+    public static void printInfo(){
+        System.out.println("#이름목록");
+        for (String name: names) {
+            if(name != null) System.out.println(name);
+        }
+    }
+
+    public static void updateName(String before, String after){
+        System.out.printf("before : %s, after : %s\n", before, after);
+        for(int i = 0; i < 5; i ++ ){
+            if(names[i].equals(before)) {
+                names[i] = after;
+                return;
+            }
+        }
+    }
+
+    public static void deleteName(String name){
+        for(int i = 0; i < 5; i ++ ){
+            if(names[i].equals(name)) names[i] = null;
+            return;
+        }
+    }
+}
---
+++ day220721/src/com/ssafy/ws/step3/NameMenuTest.java
@@ -0,0 +1,52 @@
+package com.ssafy.ws.step3;
+
+import java.util.Scanner;
+
+public class NameMenuTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userInput;
+
+        while(true){
+            printMenu();
+            userInput = sc.nextInt();
+
+            if (userInput == 5) break;
+
+            switch (userInput){
+                case 1:
+                    System.out.printf("이름입력 : ");
+                    NameMenu.addName(sc.next());
+                    break;
+                case 2:
+                    NameMenu.printInfo();
+                    break;
+                case 3:
+                    System.out.printf("기존이름입력 : ");
+                    String before = sc.next();
+
+                    System.out.printf("변경이름입력 : ");
+                    String after = sc.next();
+
+                    NameMenu.updateName(before, after);
+                    break;
+                case 4:
+                    System.out.print("삭제이름입력 : ");
+                    String name = sc.next();
+                    NameMenu.deleteName(name);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        System.out.println("-- END --");
+
+    }
+
+    public static void printMenu(){
+        System.out.println("<이름메뉴>");
+        System.out.println("1.추가 2.검색 3.수정 4.삭제 5.종료");
+        System.out.print("번호입력 ==> ");
+    }
+}
